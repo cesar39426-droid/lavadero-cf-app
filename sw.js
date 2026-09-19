22ec430067782a97e6f79756fa9c1211abc5850e
LavaderoCF Agent <lavaderocf@app.local>
2026-09-18 23:55:50 -0300

    fix(sw): force pwa cache invalidation

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
  * Versión: 1.0.0
  */
 
-const CACHE_VERSION = 'lavaderocf-v1.0.0';
+const CACHE_VERSION = 'lavaderocf-v1.1.1';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 
